f7a93442911f9090203ca86526f919fea54b1a72
Michael Bowen (Aquent LLC) <v-mibowe@microsoft.com>
2016-02-12 15:22:53 -0800

    seperate concerns

---
+++ js/shipping-info.js
@@ -0,0 +1,38 @@
+'use strict'; 
+
+$(function () {
+  var $trackingNumberForm = $('#trackingNumberForm');
+  var $shippingInfo = $('#shippingInfo');
+  var shippingDataOutput = '';
+  var itemNum = 1;
+
+  $trackingNumberForm.submit(function (e) {
+    e.preventDefault();
+    var $form = $(this);
+    var $formData = $form.serialize();
+    var $url = $form.attr('action');
+
+      // alert($formData);
+    $.post($url, $formData)
+    .done(function (data) {
+      // console.log(data.data.page);
+      for (var key in data.data) {
+        for (var key2 in data.data[key]) {
+          // console.log(data.data[key][key2]);
+          for (var key3 in data.data[key][key2]) {
+            // console.log(data.data[key][key2][key3]);
+            for (var key4 in data.data[key][key2][key3]) {
+              if (typeof data.data[key][key2][key3][key4] === 'object') {
+                // console.log(data.data[key][key2][key3][key4]);
+                shippingDataOutput += '<h2>Item Number: ' + itemNum + '</h2><ul><li>Carrier: ' + data.data[key][key2][key3][key4].slug + '</li><li>Location: ' + data.data[key][key2][key3][key4].location + '</li><li>Status: ' + data.data[key][key2][key3][key4].message + '</li></ul>';
+                itemNum += 1;
+              };
+            }
+          }
+          // console.log(shippingDataOutput);
+          $shippingInfo.html(shippingDataOutput);
+        }
+      }
+    })
+  })
+})
--- server.js
+++ server.js
@@ -2,70 +2,17 @@
 
 require('dotenv').config();
 var express = require('express');
+var shippingRouter = express.Router();
 var app = express();
 var port = process.env.PORT || 4000;
 var time = new Date();
 var clc = require('cli-color');
-var path = require('path');
-var bodyparser = require('body-parser');
-var aftership = require('aftership')(process.env.AFTERSHIP_API_KEY);
-var body = {
-  'tracking': {
-    'tracking_number': '906587618687',
-    'tracking_postal_code': 'DA15BU',
-    'tracking_ship_date': '20131231',
-    'tracking_account_number': '1234567890',
-    'slug': ['dhl', 'ups', 'fedex']
-  }
-};
 
-var query = {slug: 'dhl,ups,usps'};
+app.use(express.static(__dirname + '/'));
 
-app.get('/', function (req, res) {
-  aftership.call('get', '/couriers/all', function (err, result) {
-    if (err) {
-        console.error(err);
-        res.status(500).json({msg: 'Internal server error'});
-      } else {
-        res.json(result);
-    }
-  });
-});
+require('./routes/shipping-routes.js')(shippingRouter);
 
-app.get('/couriers', function (req, res) {
-  aftership.call('post', '/trackings', {
-    body: body
-  }, function (err, result) {
-      if (err) {
-        console.error(err);
-        res.status(500).json({msg: 'Internal server error'});
-      } else {
-        res.json(result);
-    }
-  });
-});
-
-app.get('/allshipments', function (req, res) {
-  aftership.call('get', '/trackings', {
-    query: query
-  }, 
-  function (err, result) {
-    res.json(result) ;
-  });
-});
-
-app.get('/trackpackage/:slug/:tracking_number', function (req, res) {
-  aftership.call('get', '/trackings/' + req.params.slug + '/' + req.params.tracking_number, function (err, result) {
-    if (err) {
-      console.error(err);
-      res.status(500).json({msg: 'Internal server error'});
-    } else {
-      res.json(result);
-    }
-  })
-})
-
-
+app.use('/', shippingRouter);
 
 app.listen(port, function () {
   console.log(clc.cyanBright('server started at: ' + time + ' on port: ' + port));
