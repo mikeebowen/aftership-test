aa91fad94913f0937706913883f3ded4ed94272c
Michael Bowen (Aquent LLC) <v-mibowe@microsoft.com>
2016-02-17 10:22:42 -0800

    deal with tracking numbers when checkpoints array has a length of 0

--- js/shipping-info.js
+++ js/shipping-info.js
@@ -15,14 +15,23 @@ $(function () {
 
     $.post($url, $formData)
     .done(function (data) {
-      console.log(data.data.tracking.checkpoints[data.data.tracking.checkpoints.length - 1]);
-      console.log(data.msg === 'Internal server error');
+
       if (data.msg === 'Internal server error') {
         alert('There was a problem with your tracking number, please try again');
       }
+      
       if (data.msg !== 'Internal server error') {
         for (var key in data.data.tracking) {
-          shippingDataOutput = '<div><h2>Tracking Number : ' + data.data.tracking.title + '</h2><ul><li>Carrier : ' +  data.data.tracking.slug + '</li><li>Status : ' + data.data.tracking.tag + '</li><li>Last Check in : <ul><li>Location : ' + data.data.tracking.checkpoints[data.data.tracking.checkpoints.length - 1].location + '</li><li>Time : ' + data.data.tracking.checkpoints[data.data.tracking.checkpoints.length - 1].checkpoint_time + '</li><li>Status : ' + data.data.tracking.checkpoints[data.data.tracking.checkpoints.length - 1].tag + '</li></ul></li><li>Weight : ' + data.data.tracking.shipment_weight + ' ' + data.data.tracking.shipment_weight_unit + '</li></ul><button type="button" onclick="$(this).parent().remove()">Remove</button></div>';
+          shippingDataOutput = '<div><h2>Tracking Number : ' + data.data.tracking.title + '</h2><ul><li>Carrier : ' +  data.data.tracking.slug + '</li><li>Status : ' + data.data.tracking.tag + '</li>';
+
+          if (data.data.tracking.checkpoints.length > 0) {
+            shippingDataOutput += '<li>Last Check in : <ul><li>Location : ' + data.data.tracking.checkpoints[data.data.tracking.checkpoints.length - 1].location + '</li><li>Time : ' + data.data.tracking.checkpoints[data.data.tracking.checkpoints.length - 1].checkpoint_time + '</li><li>Status : ' + data.data.tracking.checkpoints[data.data.tracking.checkpoints.length - 1].tag + '</li></ul></li><li>Weight : ' + data.data.tracking.shipment_weight + ' ' + data.data.tracking.shipment_weight_unit + '</li></ul><button type="button" onclick="$(this).parent().remove()">Remove</button></div>';
+
+          }
+
+          if (data.data.tracking.checkpoints.length === 0) {
+            shippingDataOutput += '</ul><button type="button" onclick="$(this).parent().remove()">Remove</button></div>';
+          }
         }
 
       }
