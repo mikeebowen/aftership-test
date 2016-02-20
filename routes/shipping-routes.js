bf82cbef1f0a01363afecd263086493cf52d771a
Michael Bowen (Aquent LLC) <v-mibowe@microsoft.com>
2016-02-19 16:49:26 -0800

    add route to use slug for tracking

--- routes/shipping-routes.js
+++ routes/shipping-routes.js
@@ -51,5 +51,41 @@ module.exports = function (router) {
     });
   });
 
+  router.get('/trackshipment2/:trackingNumber', function (req, res) {
+    var reqBody = {'tracking' : {'tracking_number': req.params.trackingNumber, 'slug' : ''}};
+    var shippingInfo;
+    var theSlug = '';
+    var theTrackingNumber = req.params.trackingNumber;
+    
+    aftership.call('post', '/trackings', {
+      body: reqBody
+    }, function (err, result) {
+      
+      if (err && err.code !== 4003) {
+        console.log(err);
+        res.json({msg: 'Internal server error'}).status(500);
+      }
+
+      if (err && err.code === 4003) {
+        reqBody.tracking.slug = err.data.tracking.slug; 
+      } 
+      if (!err) {
+        reqBody.tracking.slug = result.data.tracking.slug;
+      }
+      if (!err || err.code === 4003) {
+        aftership.call('get', '/trackings/' + reqBody.tracking.slug + '/' + reqBody.tracking.tracking_number, function (err, result) {
+          if (err) {
+            console.log(err);
+            res.json({msg: 'Internal server error'}).status(500);
+          } 
+          if (!err) {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.json(result).status(202);            
+          }
+        });
+      }
+    });
+  });
+
  
 }
